69c4c15a18ce9b0fe0c597e5542f1bb9a4695753
sjc54 <47803292+sjc54@users.noreply.github.com>
2019-05-08 20:16:27 +1000

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<p>This website was produced by students in the Faculty of Arts & Design, University of Canberra. 
-The text and images reproduced on this site were generously provided by <a href="http://www.canberrahouse.com.au">Canberra House</a>
-</p>
-
-</body>
-
-</html>
